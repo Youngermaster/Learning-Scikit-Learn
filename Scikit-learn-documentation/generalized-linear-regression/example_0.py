513601cfadc60fbb8f175096733533a2fe58d61a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-09-30 15:03:57 -0500

    Examples of generalized linear regression added

---
+++ Scikit-learn-documentation/generalized-linear-regression/example_0.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+model = Pipeline([('poly', PolynomialFeatures(degree=3)),
+                  ('linear', LinearRegression(fit_intercept=False))])
+# fit to an order-3 polynomial data
+x = np.arange(5)
+y = 3 - 2 * x + x ** 2 - x ** 3
+
+model = model.fit(x[:, np.newaxis], y)
+model.named_steps['linear'].coef_
---
+++ Scikit-learn-documentation/generalized-linear-regression/example_1.py
@@ -0,0 +1,18 @@
+from sklearn.linear_model import Perceptron
+from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+y = X[:, 0] ^ X[:, 1]
+print(y)
+
+X = PolynomialFeatures(interaction_only=True).fit_transform(X).astype(int)
+print(X)
+
+clf = Perceptron(fit_intercept=False, max_iter=10, tol=None,
+                 shuffle=False).fit(X, y)
+
+print(clf.predict(X))
+
+print(clf.score(X, y))
